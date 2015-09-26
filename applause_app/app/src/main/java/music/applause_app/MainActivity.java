ed1ad2c7141de5a7fc87a14646562bf7d32247a6
dyangman <dennis.sihang.yang@gmail.com>
2015-09-26 10:11:38 -0500

    Revert "web socket for client side"
    This reverts commit 819f763916579b1d3bed3830cdf76259f658edc1.

--- applause_app/app/src/main/java/music/applause_app/MainActivity.java
+++ applause_app/app/src/main/java/music/applause_app/MainActivity.java
@@ -1,12 +1,9 @@
 package music.applause_app;
 
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,46 +34,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    private void connectWebSocket() {
-        URI uri;
-        try {
-            uri = new URI("ws://websockethost:8080");
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        mWebSocketClient = new WebSocketClient(uri) {
-            @Override
-            public void onOpen(ServerHandshake serverHandshake) {
-                Log.i("Websocket", "Opened");
-                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
-            }
-
-            @Override
-            public void onMessage(String s) {
-                final String message = s;
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        TextView textView = (TextView) findViewById(R.id.messages);
-                        textView.setText(textView.getText() + "\n" + message);
-                    }
-                });
-            }
-
-            @Override
-            public void onClose(int i, String s, boolean b) {
-                Log.i("Websocket", "Closed " + s);
-            }
-
-            @Override
-            public void onError(Exception e) {
-                Log.i("Websocket", "Error " + e.getMessage());
-            }
-        };
-        mWebSocketClient.connect();
-    }
 }
-
